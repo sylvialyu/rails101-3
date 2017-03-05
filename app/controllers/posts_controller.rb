90d417fee2d5fe9ed0e76849e67002ff19a2d70d
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-03-05 23:04:34 +0800

    completed 6.3 step4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(group), notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to group_path(group), alert: "Post Deleted"
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
