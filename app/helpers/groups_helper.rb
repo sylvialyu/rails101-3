8586cac33e3b8cd91c3b132100a76d52b83156a9
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-03-06 20:53:20 +0800

    use self delimit helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,10 @@
 module GroupsHelper
+  def render_group_description(group)
+    simple_format(group.description)
+  end
+
+  def render_post_description(post)
+    simple_format(post.content)
+  end
+
 end
